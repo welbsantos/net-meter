fbd53b5174a6ddd599b0b2220668a27d7d2a0794
Welb Santos <welb.santos@gmail.com>
2019-05-15 23:25:34 -0300

    initial

--- index.js
+++ index.js
@@ -6,34 +6,36 @@ let appIcon = null;
 let win = null;
 
 app.on('ready', () => {
-        win = new BrowserWindow({show: false});
-        appIcon = new Tray(iconPath);
-        var contextMenu = Menu.buildFromTemplate([
-            {
-            label: "Download:",
-            click: function() {
-                
-                }
-            },
-            {
-            label: "Upload:",
-            click: function() {
-                
-                }
-            },
-            {type: 'separator'},
-            { label: 'Quit',
-            accelerator: 'CommandOrControl+Q',
-            selector: 'terminate:',
-            click: function() {
-                    app.quit();
-                    app.exit();
-                }
+    win = new BrowserWindow({show: false});
+    appIcon = new Tray(iconPath);
+    var contextMenu = Menu.buildFromTemplate([
+        {
+        label: "Download:",
+        click: function() {
+            
             }
-        ]);
-        appIcon.setToolTip('net-meter');
-        appIcon.setContextMenu(contextMenu);
-    });
+        },
+        {
+        label: "Upload:",
+        click: function() {
+            
+            }
+        },
+        {type: 'separator'},
+        { label: 'Quit',
+        accelerator: 'CommandOrControl+Q',
+        selector: 'terminate:',
+        click: function() {
+                app.quit();
+                app.exit();
+            }
+        }
+    ]);
+    appIcon.setToolTip('net-meter');
+    appIcon.setContextMenu(contextMenu);
+    
+});
+
 app.on('quit', () => {
     app.quit();
     app.exit();
